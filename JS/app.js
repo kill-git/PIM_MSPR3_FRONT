a2b6f31766b7e95b4443131ffa1164e17837403c
kill-git <killiansayan@gmail.com>
2023-07-28 09:42:20 +0200

    first commit

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+function redirectToCreateAccountPage() {
+    window.location.href = "SignIn.html";
+  }
+
+  function redirectToConnectPage() {
+    window.location.href = "Login.html";
+  }
+
+  function redirectToHomePage() {
+    window.location.href = "Produit.html";
+  }
